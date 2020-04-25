8160946415942ab1d99fb5c796b6b9cc8b33c232
mwataru-pro <m1130wata@gmail.com>
2020-04-25 15:21:29 +0900

    [Finish] 4-1

--- db/migrate/20200425024259_create_blogs.rb
+++ db/migrate/20200425024259_create_blogs.rb
@@ -9,3 +9,4 @@ class CreateBlogs < ActiveRecord::Migration[5.2]
     end
   end
 end
+ 
--- test/models/blog_test.rb
+++ test/models/blog_test.rb
@@ -3,5 +3,5 @@ require 'test_helper'
 class BlogTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
-  # end
+  # end 
 end
